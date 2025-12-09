0664a26808cb366c3d876bfb27d5a960ce0e8de7
donjon483 <70143736+donjon483@users.noreply.github.com>
2025-12-09 16:38:31 +0530

    final commit

--- src/main/java/com/example/rbac_user_management_service/Config/KafkaProducerConfig.java
+++ src/main/java/com/example/rbac_user_management_service/Config/KafkaProducerConfig.java
@@ -1,6 +1,8 @@
 package com.example.rbac_user_management_service.Config;
 
+
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -13,18 +15,33 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 @Configuration
 public class KafkaProducerConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
     @Bean
     public ProducerFactory<String, String> producerFactory() {
 
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(config);
     }
+
+
+//    @Bean
+//    public ProducerFactory<String, String> producerFactory() {
+//
+//        Map<String, Object> config = new HashMap<>();
+//
+//        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//
+//        return new DefaultKafkaProducerFactory<>(config);
+//    }
 
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate() {
